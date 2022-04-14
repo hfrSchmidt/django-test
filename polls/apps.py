b1c00527ca2a01f7a1d0f794c0374e2e7ac31e5a
hfrSchmidt <9467795+hfrSchmidt@users.noreply.github.com>
2022-04-14 17:35:06 +0200

    Rename test_app to follow the django tutorial more closely

--- polls/apps.py
+++ polls/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class TestAppConfig(AppConfig):
+class PollsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'test_app'
+    name = 'polls'
