eaca19cf0c8b69d748846883ce8847856ba2cf56
hfrSchmidt <9467795+hfrSchmidt@users.noreply.github.com>
2022-04-17 17:20:31 +0200

    Modify views to render html templates

--- polls/views.py
+++ polls/views.py
@@ -1,17 +1,19 @@
 from curses.ascii import HT
-import imp
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import get_object_or_404, render
+from django.http import Http404, HttpResponse
 from .models import Question
 
 
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    output = ', '.join([q.question_text for q in latest_question_list])
-    return HttpResponse(output)
+    context = {
+        'latest_question_list': latest_question_list
+    }
+    return render(request, 'polls/index.html', context)
 
 def detail(request, question_id):
-    return HttpResponse("You are looking at question %s" % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/detail.html', {'question': question})
 
 def results(request, question_id):
     response = "You are looking at the results of question %s."
